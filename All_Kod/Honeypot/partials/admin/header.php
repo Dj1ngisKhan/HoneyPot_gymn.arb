2935bf2aa205d2b2d5ea460d09442d16d84cae12
Temujin <halloklish@gmail.com>
2025-06-04 14:24:20 +0200

    Create header.php

---
+++ All_Kod/Honeypot/partials/admin/header.php
@@ -0,0 +1,38 @@
+<?php
+include __DIR__ . "/../../init.php";
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Admin Dashboard</title>
+    <!-- Bootstrap CSS -->
+    <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+    >
+
+
+    <style>
+
+        html, body {
+            height: 100%;
+            margin: 0;
+        }
+
+        body {
+            display: flex;
+            flex-direction: column;
+        }
+
+        main {
+            flex: 1;
+        }
+
+    </style>
+</head>
+<body>
