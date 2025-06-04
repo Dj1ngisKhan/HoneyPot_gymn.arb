ddc0a772aafab4c5ec0d1ef4a46f24c57f3b17fd
Temujin <halloklish@gmail.com>
2025-06-04 14:23:35 +0200

    Create hero.php

---
+++ All_Kod/Honeypot/partials/hero.php
@@ -0,0 +1,8 @@
+<!-- Header Section -->
+<header class="bg-dark text-white py-5">
+    <div class="container">
+        <h1 class="display-4">Welcome to the Admin Dashboard</h1>
+        <p class="lead">
+        </p>
+    </div>
+</header>
