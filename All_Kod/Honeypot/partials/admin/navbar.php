4c3ba73cf35202e04ab64358e2f2a0c7093aef5b
Temujin <halloklish@gmail.com>
2025-06-04 14:24:35 +0200

    Create navbar.php

---
+++ All_Kod/Honeypot/partials/admin/navbar.php
@@ -0,0 +1,36 @@
+<!-- Navigation Bar -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="/bruh/website/index.php">Admin Dashboard</a>
+        <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+        >
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div
+            class="collapse navbar-collapse"
+            id="navbarNav"
+        >
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="admin.php">Dashboard</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/bruh/website/admin.php">Logga in</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/bruh/website/signup.php">Skapa konto</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/bruh/website/index.php">Hem</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
